f31f48f00e957af78c3194c8ec1b3863a20ba790
fojomars <junior.mars125@gmail.com>
2020-03-20 18:24:44 +0100

    Terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {environment} from '../environments/environment';
 import {ServicioDeAutentService} from './servicio-de-autent.service';
 import {FireDBService} from './fire-db.service';
 
